c2f31daf55da2c2fb79a39a4d6b8c7988bb0a15d
XXI <ngmount2021@gmail.com>
2025-06-15 12:31:43 -0400

    Fixed error in warning message implementation

--- src/pyhund/argparse.py
+++ src/pyhund/argparse.py
@@ -3,38 +3,59 @@ from sys import argv, exit
 
 def display_help() -> None:
     help_text = """
+[PyHund:Help ~]::
+
+Usage: python3 pyhund.py <username1> <username2> ... [options]
+
+Options (prefix with - or /):
+\th | help\t\tDisplay this help message
+\tstdout:<format>\t\tSpecify output format (default[stdout], json, txt, pipe)
+\tverbose\t\t\tEnable verbose output (only for stdout=default)
+\tdebug\t\t\tEnable debug output (only for stdout=default/txt)
     """
     print(help_text)
     exit(0)
 
 def parse_args() -> dict:
+    """
+    Parse provided command line arguments into a configuration hash map
+    :return: Hash map containing all parsed configuration options
+    :rtype: dict
+    """
 
-    if len(argv) < 2:
-        display_help()
+    if len(argv) < 2: display_help()
 
     parsed_config = {
         "unames": [],
         "stdout": "default",
+        "verbose": False,
+        "debug": False,
     }
     
     for arg in argv[1:]:
+
+        # If current arg is a flag or option then process that flag/option
         if arg.startswith('-') or arg.startswith('/'):
             arg = arg.lstrip('-').lstrip('/')
 
-            if arg.split(':')[0] in ('h', 'help'):
-                display_help()
+            # If requesting help then display and exit
+            if arg.split(':')[0] in ('h', 'help'): display_help()
 
-            if arg.split(':')[0] in parsed_config.keys() and arg.split(':')[0] != "unames":
-                parsed_config[arg.split(':')[0]] = arg.split(':')[1] if ':' in arg else None
+            if arg.split(':')[0] != "unames":
+                parsed_config[arg.split(':')[0]] = arg.split(':')[1] if ':' in arg else True
+            
+            continue
         
-        parsed_config['unames'].append(arg.strip)
+        parsed_config['unames'].append(arg.strip())
 
-    if not parsed_config['unames']:
+    # If no usernames are provided, no operations can be performed, so exit
+    if len(parsed_config['unames']) == 0:
         print("[Err ~]:: Must provide at least one username")
         exit(1)
 
+    # If user has provided unknown stdout format then warn of improper option provided and run with stdout=default
     if parsed_config['stdout'] not in ['default', 'json', 'txt', 'pipe']:
         print('[Warn ~]:: Invalid output format specified, must be one of (default, json, txt, pipe)\n           Reverting to default and continuing')
         parsed_config['stdout'] = 'default'
-    
+
     return parsed_config
