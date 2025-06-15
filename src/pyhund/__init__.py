820aedfb742fa81c5088e9bbc42377cf8a3879a6
XXI <ngmount2021@gmail.com>
2025-06-15 12:53:09 -0400

    Updated init file to add verbose debugging of command line args and dependancy version metadata

--- src/pyhund/__init__.py
+++ src/pyhund/__init__.py
@@ -1,26 +1,17 @@
 
 from pyhund.argparse import parse_args
+from pyhund.scanning import run_scan, site_index_meta
 
 def run():
     
     config = parse_args()
 
-    scan_results = {
-        # Metadata on program run as a whole
-        "Meta": [0,0,0,0], # [Total Hits, Total Misses, Total Unknown, Total Visited]
-        
-        # Results per username in the format as follows
-        # [sitename, url, response_code, validation_status, validation_method, hit_status]
-        #     # Sitename: Display name of the visited site
-        #     # URL: URL used by the program to attempt to find the account and validate for authentication
-        #     # Response Code: HTTP response code returned by the site
-        #     # Validation Status: Status of validation on authenticity of site response (e.g., "Valid", "Invalid", "Unknown")
-        #     # Validation Method: Method used to validate the response (e.g., "RegEx", "Status", "URL")
-        #     # Hit Status: Status of the hit (e.g., "Hit", "Miss", "Unknown")
-        "Results": {}
-    }
+    # If verbose or debug mode active, display version and config info
+    if config['verbose'] or config['debug']:
+        print("\n[PyHund:Config ~]:: Configuration Data")
+        print("---------------------------------------")
+        for k, v in config.items():
+            print("{:<16}:\t {}".format(k.capitalize(), v))
+        print("Manifest Version:\t {}".format(site_index_meta['version']))
 
-    for uname in set(config['unames']):
-        
-        # Make a new user entry in the results object
-        scan_results["Results"][uname.lower()] = []
+    scan_object = run_scan(config=config)
