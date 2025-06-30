348f393252b596278ec887c5ac18e9a46b9ff970
XXI <ngmount2021@gmail.com>
2025-06-29 23:23:00 -0400

    Implemented basic plugin manager features to load plugins

--- src/pyhund/plugin_manager.py
+++ src/pyhund/plugin_manager.py
@@ -1,5 +1,61 @@
-
+from os import listdir
+from plugins import Plugin
 class PluginManager:
 
-    def __init__(self):
-        pass
+    def __init__(self, config:dict):
+        self.config:dict = config
+        self.plugins_index:list[Plugin] = []
+
+        # Load plugins on initialization
+        self.load_plugins()
+
+    def load_plugins(self) -> None:
+        """
+        Load Pluginds
+        This function locates and loads all plugins from the 'src/plugins' directory.
+        """
+
+        # Iterate through the plugin index and import each plugin
+        for plugin in listdir('/home/xxi/Projects/In-Progress/Tools/PyHund-R7.1/src/plugins'):
+
+            # Fast rejection of non-Python files and __* files
+            if not plugin.endswith('.py') or plugin.startswith('__'):
+                continue
+
+            if ( module := self._load_plugin(plugin)) is not None:
+                self.plugins_index.append(module())
+
+    def _load_plugin(self, plugin_name:str) -> Plugin:
+        """
+        Load Plugin <Private>
+        This function attempts to import a single plugin based on its name ( case sensitive ).
+        :param plugin_name: The name of the plugin to load, must be a valid Python file in the plugins directory.
+        :return: An instance of the plugin class if successful, None otherwise.
+        """
+
+        try:
+
+            # If debugging or verbose mode is enabled, print the plugin being loaded
+            if self.config['debug'] or self.config['verbose']:
+                print(f"[PyHund:PluginManager ~]:: Loading plugin {plugin_name[:-3]}...")
+
+            # Attempt to get the plugin class "<Plugin_Name>Plugin" from module "<Plugin_Name>"
+            return getattr(
+                    getattr(__import__(
+                        "plugins.{}".format(plugin_name[:-3])), 
+                        plugin_name[:-3]
+                    ), 
+                    plugin_name[:-3] + "Plugin"
+                )
+        
+        # If module cannot be imported, catch the ImportError
+        # Report the error if debugging or verbose mode is enabled
+        except ImportError as e:
+            if self.config['debug'] or self.config['verbose']:
+                print(f"[PyHund:PluginManager:Err ~]:: Failed to load plugin {plugin_name[:-3]}: {e}")
+
+            return None
+        
+
+        
+                
